1a75f150ff346ea72e05a9ed2377c95410d6a98f
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-06-11 14:25:14 +0300

    Izveidots programmas pamats/sākums

--- Tests.cpp
+++ Tests.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+void jautajumi();
+
 int main(){
+	cout<<"\t\t\t\t\t\t\tTests\n\nTema:\nMainigie, to datu tipi, pamatdarbibas darbam ar tiem programmesanas valoda C++";
+	
+	cout<<"\n\nPiespiediet jebkuru taustinu, lai saktu testu!";
+	
+	getch();
+	
+	system("CLS");
+	
+	jautajumi();
+	
+	return 0;
+}
+
+void jautajumi(){
 	
 }
+
