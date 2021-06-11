25c52b55198ce93b61cfd171542af94d1a24d022
Endijs Dārznieks <endijsdarznieks@gmail.com>
2021-06-11 18:06:26 +0300

    Izdomāta pamata doma, kā pats tests strādās

--- Tests.cpp
+++ Tests.cpp
@@ -20,6 +20,27 @@ int main(){
 }
 
 void jautajumi(){
+	int x = 1, a;
+	bool answer = false;
 	
+	switch(x){
+		
+		case 1:
+			do{
+			cout<<"\n\t\t1. Jautajums\n";
+			cout<<"\n\t1) Opcija1";
+			cout<<"\n\t2) Opcija2";
+			cout<<"\n\t3) Opcija3";
+			cout<<"\n\t4) Opcija4";
+			cout<<"\n\n\t\tAtbilde:";
+			cin>>a;
+			
+			if(a == 2 || a == 4)
+				answer = true;
+			
+			}while(answer == false);
+			
+			x = 2;
+	}
 }
 
